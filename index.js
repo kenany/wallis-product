e0e84470de2adf70386f404f193415c70689bb14
Kenan Yildirim <kenan@kenany.me>
2021-08-09 10:47:08 -0400

    chore(deps): @kenan/eslint-config@9.0.4
    Closes https://github.com/KenanY/wallis-product/pull/50

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+/**
+ * @param {number} n
+ * @returns {number}
+ */
 function wallis(n) {
-  var product = 1;
-  for (var i = 1; i <= n; i++) {
+  let product = 1;
+  for (let i = 1; i <= n; i++) {
     product *= (2 * i / (2 * i - 1)) * (2 * i / (2 * i + 1));
   }
   return 2 * product;
--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
-var test = require('tape');
-var isFunction = require('lodash.isfunction');
+const test = require('tape');
+const isFunction = require('lodash.isfunction');
 
-var wallis = require('../');
+const wallis = require('../');
 
 test('exports a function', function(t) {
   t.plan(1);
@@ -9,9 +9,9 @@ test('exports a function', function(t) {
 });
 
 test('approaches pi', function(t) {
-  var last = 1;
-  var w;
-  for (var i = 0; i < 101; i++) {
+  let last = 1;
+  let w;
+  for (let i = 0; i < 101; i++) {
     w = wallis(i);
 
     // greater than last approximation
